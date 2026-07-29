36a6f9a1f2076b3ab7a2d7ee8789309fec7a0f87
Ravikumar Amireddy <amireddyravi@gmail.com>
2026-07-29 22:47:19 +0530

    shell

--- 14-color.sh
+++ 14-color.sh
@@ -1,40 +1,40 @@
-#!/bin/bash
+#1/bin/bash
 
 USERID=$(id -u)
-LOGS_DIR=/var/log/shell-script
-LOGS_FILE="$LOGS_DIR/$0.log"
-TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+LOGS_DIR=/var/log/shell-script
+LOG_FILE="$LOGS_DIR/$0.log" # /var/log/shell-script/*.sh.log
 
-# Check root access or not
-if [ $USERID -ne 0 ]; then
-    echo "Please run this script with root access"
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with super user"
     exit 1
 fi
-
-# first arg -> what are you trying to install
-# second arg -> exit code
-VALIDATE(){
+# 
+function VALIDATE(){
     if [ $2 -ne 0 ]; then
-        echo -e "$TIMESTAMP [ERROR] Installing $1 is ... $R FAILED $N" | tee -a $LOGS_FILE
+        echo -e "$TIMESTAMP [ERROR] $1 installing is...  $R FAILED $N" | tee -a $LOG_FILE
         exit 1
-    else
-        echo -e "$TIMESTAMP [INFO] Installing $1 is ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    else 
+        echo -e "$TIMESTAMP [INFO] $1 is installed... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
-
-
 for package in $@
-do
-    echo "$TIMESTAMP [INFO] Installing $package"
-    dnf list installed $package &>> $LOGS_FILE
-    if [ $? -ne 0 ]; then
-        dnf install $package -y &>> $LOGS_FILE
-        VALIDATE "Installing $package" $?
+ do 
+    # echo $package
+    dnf list installed $package &>> $LOG_FILE
+    if [ $? -eq 0 ]; then
+        echo -e "$TIMESTAMP [INFO] $package is already Installed.. $Y SKIPPING $N" | tee -a $LOG_FILE
     else
-        echo -e "$TIMESTAMP [INFO] $package already installed ... $Y SKIPPING $N"
+        echo "Installing $package"
+        dnf install $package -y &>> $LOG_FILE
+        VALIDATE $package $?
     fi
-done
+ done
+
+ echo "$Y SKIPPING"
