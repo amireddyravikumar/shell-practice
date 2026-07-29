e89e3f6a0385eea3a44b85680b16d78d45c71375
Ravikumar Amireddy <amireddyravi@gmail.com>
2026-07-29 22:40:10 +0530

    shell

--- 14-color.sh
+++ 14-color.sh
@@ -29,7 +29,7 @@ for package in $@
     # echo $package
     dnf list installed $package &>> $LOG_FILE
     if [ $? -eq 0 ]; then
-        echo -e "$TIMESTAMP [INFO] $package is already Installed..  $Y SKIPPING" | tee -a $LOG_FILE
+        echo "$TIMESTAMP [INFO] $package is already Installed.. SKIPPING" | tee -a $LOG_FILE
     else
         echo "Installing $package"
         dnf install $package -y &>> $LOG_FILE
