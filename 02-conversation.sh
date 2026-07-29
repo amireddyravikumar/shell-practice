f3945f9bd99637235b3a9e2a2f959ef21c843be2
Ravikumar Amireddy <amireddyravi@gmail.com>
2026-07-29 13:17:18 +0530

    shell

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-person1="ravi"
-person2="kumar"
+person1=ravi
+person2=kumar
 
-echo 'Hi, I am $person1'
-echo 'Hellow, I am $person2' 
+echo "Hi, I am $person1"
+echo "Hellow, I am $person2" 
