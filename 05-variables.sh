fc70caf18501bb533ca1c09bc1d25b490f821ad1
Ravikumar Amireddy <amireddyravi@gmail.com>
2026-07-29 13:29:52 +0530

    shell

---
+++ 05-variables.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+TIMESTAMP=$(date)
+
+echo "time is $TIMESTAMP"
+START_TIMESTAMP=$(date +%s)
+sleep 10
+END_TIMESTAMP=$(date +%s)
+
+TOTAL_DIFF=END_TIMESTAMP-START_TIMESTAMP
+echo "TOTAL_DIFF is $TOTAL_DIFF"
