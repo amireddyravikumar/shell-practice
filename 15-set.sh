12df638e36e00006d90758841760ff1ea283c6fb
Ravikumar Amireddy <amireddyravi@gmail.com>
2026-07-29 23:08:39 +0530

    shell

---
+++ 15-set.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+echo "hi"
+echo $BASH_COMMAND
+dasdsa
+echo "continuing"
---
+++ 16-set.sh
@@ -0,0 +1,33 @@
+#1/bin/bash
+
+set -e
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+trap 'echo "error at $LINENO", command: $BASH_COMMAND"' ERR
+
+LOGS_DIR=/var/log/shell-script
+LOG_FILE="$LOGS_DIR/$0.log" # /var/log/shell-script/*.sh.log
+
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with super user"
+    exit 1
+fi
+   
+for package in $@
+ do 
+    # echo $package
+    dnf list installed $package &>> $LOG_FILE
+    if [ $? -eq 0 ]; then
+        echo -e "$TIMESTAMP [INFO] $package is already Installed.. $Y SKIPPING $N" | tee -a $LOG_FILE
+    else
+        echo "Installing $package"
+        dnf install $package -y &>> $LOG_FILE
+       # VALIDATE $package $?
+    fi
+ done
