22092e55373cb240cc3a4995ab79196332a5bf36
Ravikumar Amireddy <amireddyravi@gmail.com>
2026-07-29 21:18:49 +0530

    shell

--- 11-logs.sh
+++ 11-logs.sh
@@ -2,7 +2,7 @@
 
 USERID=$(id -u)
 
-LOGS_DIR=/home/ec2-user/shell-logs
+LOGS_DIR=/var/log/shell-logs
 LOG_FILE="$LOGS_DIR/$0.log" # /home/ec2-user/shell-logs/11-logs.sh.log
 
 if [ $USERID -ne 0 ]
@@ -13,28 +13,28 @@ fi
 # 
 function VALIDATE(){
     if [ $2 -ne 0 ]; then
-        echo "$1 installing is... FAILED"
+        echo "$1 installing is... FAILED" | tee -a $LOG_FILE
         exit 1
     else 
-        echo "$1 is installed... SUCCESS"
+        echo "$1 is installed... SUCCESS" | tee -a $LOG_FILE
     fi 
 }
 
 
 dnf list installed mysql &>> $LOG_FILE
 if [ $? -eq 0 ]; then
-   echo "MySQL is already Installed.. SKIPPING"
+   echo "MySQL is already Installed.. SKIPPING"  | tee -a $LOG_FILE
 else
-   echo "Installing MySQL"
+   echo "Installing MySQL"  | tee -a $LOG_FILE
    dnf install mysql -y &>> $LOG_FILE
    VALIDATE Mysql $?
 fi
 
 dnf list installed nginx &>> $LOG_FILE
 if [ $? -eq 0 ]; then
-   echo "nginx is already Installed.. SKIPPING"
+   echo "nginx is already Installed.. SKIPPING"  | tee -a $LOG_FILE
 else
-   echo "Installing nginx" 
+   echo "Installing nginx" | tee -a $LOG_FILE
    dnf install nginx -y &>> $LOG_FILE
    VALIDATE Mysql $?
 fi
