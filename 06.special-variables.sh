e00e5b65d05906e69ded9d40c58969be7ac91739
Ravikumar Amireddy <amireddyravi@gmail.com>
2026-07-29 15:01:02 +0530

    shell

---
+++ 06.special-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "All variabls are: $@"
+echo "No of variables passed: $#"
+echo "First variable: $1"
+echo "Script Name: $0"
+echo "who is running user Name: $USER"
+echo "Which directory: $PWD"
+echo "Home directory: $HOME"
