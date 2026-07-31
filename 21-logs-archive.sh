0b276df522a4ec34aa737fc1f13266aeebcbfa92
Ravikumar Amireddy <amireddyravi@gmail.com>
2026-07-31 22:08:01 +0530

    shell practice

---
+++ 21-logs-archive.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=$(2:-14)
+
+if [ -z "$SOURCE_DIR" ] || [ -z "$DEST_DIR" ]; then
+    echo "ERROR:: eother source directory or destintation directory empty"
+    echo "USAGE:: $0 [source_dir] [dest_dir] [days(optional)::default 14]"
+    exit 0
+fi
+
+if [ ! -d "$SOURCE_DIR" ]; then
+    echo "Source directory:: $SOURCE_DIR does not exists"
+    exit 1
+fi
+
+if [ ! -d "$DEST_DIR" ]; then
+    echo "Source directory:: $DEST_DIR does not exists"
+    exit 1
+fi
