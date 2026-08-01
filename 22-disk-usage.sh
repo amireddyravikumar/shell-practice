bd990f1c8df78225ccfd3eaf4966ac01462b4918
Ravikumar Amireddy <amireddyravi@gmail.com>
2026-08-01 13:09:29 +0530

    shell-practice

---
+++ 22-disk-usage.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep -v Filesystem)
+while IFS= read -r line
+do 
+    USAGE=$(echo $line | awk '{print $6}' cut -d "%" -f1)
+    echo $line
+done <<< "$DISK_USAGE"
