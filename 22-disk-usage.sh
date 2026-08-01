4dde6af394cff8fde1c3157ab04c08d7ba8f531c
Ravikumar Amireddy <amireddyravi@gmail.com>
2026-08-01 14:20:00 +0530

    shell-practice

--- 22-disk-usage.sh
+++ 22-disk-usage.sh
@@ -2,6 +2,7 @@
 
 DISK_TRESHOLD=10
 DISK_USAGE=$(df -hT | grep -v Filesystem)
+SERVER_IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 while IFS= read -r line
 do 
     USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
@@ -12,3 +13,5 @@ do
     fi
 done <<< "$DISK_USAGE"
 echo -e $MESSAGE
+
+sh mail.sh "DevOps Team" "High Disk Usage" "$SERVER_IP" "$MESSAGE" "amireddyravi@gmail.com" "High Disk Usage Alert"
