7c96e6e1a88126bd96e9a9872fc55a04bc0847e5
Ravikumar Amireddy <amireddyravi@gmail.com>
2026-08-01 13:14:22 +0530

    shell-practice

--- 22-disk-usage.sh
+++ 22-disk-usage.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+DISK_TRESHOLD=70
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 while IFS= read -r line
 do 
     USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
     PARTITION=$(echo $line | awk '{print $7}')
     echo $PARTITION
-    
+    if [ "$DISK_TRESHOLD" -ge "$USAGE"] ; then
+        echo "Disk Usage is High on $PARTITION: $USAGE"
+    if 
 
 done <<< "$DISK_USAGE"
